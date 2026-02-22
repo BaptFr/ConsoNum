5c4eb058692f6916b1c97f9ee589631604a2acb5
B <baptistesalazar@gmail.com>
2026-02-23 00:22:56 +0100

    feat: frontend  login form features added

--- frontend/components/features/auth/LoginForm.tsx
+++ frontend/components/features/auth/LoginForm.tsx
@@ -3,17 +3,51 @@
 import { Input, Button, Link } from "@heroui/react";
 import { useState } from "react";
 import NextLink from "next/link";
+import { useRouter } from "next/navigation";
 
 export const LoginForm = () => {
+  const router = useRouter()
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setError("");
+
+    if (!email || !password) {
+      setError("Tous les champs sont requis.");
+      return;
+    }
+
     setIsLoading(true);
-    // API Simul  TODO
-    setTimeout(() => setIsLoading(false), 2000);
-    console.log("Submit Login");
+    //TODO URL
+    try {
+      const res = await fetch(`http://localhost:8000/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: email, password }),
+      });
+
+      if (!res.ok) {
+        const data = await res.json();
+        throw new Error(data.message || "Identifiants incorrects.");
+      }
+
+      const data = await res.json();
+      localStorage.setItem("token", data.token);
+
+      router.push("/");
+
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
+    
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-5 ">
@@ -24,6 +58,8 @@ export const LoginForm = () => {
         placeholder="votre@email.com"
         labelPlacement="outside"
         radius="sm"
+        value={email}
+        onValueChange={setEmail}
         classNames={{
           inputWrapper: "bg-transparent bg-default  p-2 rounded-md ",
         }}
@@ -36,6 +72,8 @@ export const LoginForm = () => {
         placeholder="*********"
         labelPlacement="outside"
         radius="md"
+        value={password}
+        onValueChange={setPassword}
         classNames={{
           inputWrapper: "bg-transparent bg-default p-2 rounded-md",
         }}
@@ -46,6 +84,10 @@ export const LoginForm = () => {
           Mot de passe oublié ?
         </Link>
       </div>
+
+      {error && (
+        <p className="text-danger text-sm text-center">{error}</p>
+      )}
 
       <Button
         type="submit"
