7c5451101bc2b7849c0772ba370e59c4eb880afb
B <baptistesalazar@gmail.com>
2026-02-25 20:28:17 +0100

    tests: AuthControllerTest set up - OK

--- frontend/components/features/auth/LoginForm.tsx
+++ frontend/components/features/auth/LoginForm.tsx
@@ -32,7 +32,7 @@ export const LoginForm = () => {
       const res = await fetch(`http://localhost:8000/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username: email, password }),
+        body: JSON.stringify({ email, password }),
       });
 
       if (!res.ok) {
