041c1947273e8cb4552c184de0e089981a301c2b
B <baptistesalazar@gmail.com>
2026-03-02 00:42:12 +0100

    fix: nginx default and auth login var correction

--- frontend/components/features/auth/LoginForm.tsx
+++ frontend/components/features/auth/LoginForm.tsx
@@ -29,7 +29,7 @@ export const LoginForm = () => {
     setIsLoading(true);
     //TODO URL
     try {
-      const res = await fetch(`http://localhost:8000/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
