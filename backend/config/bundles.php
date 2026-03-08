80361dc10eedb7406ff56ed8e3d3204fda5dd1b4
B <baptistesalazar@gmail.com>
2026-03-08 16:04:00 +0100

    feat: install MonologBundle for logging

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
