a2a19c61cd7daeaeb583cf8e513cfd371dc0b7cd
B <baptistesalazar@gmail.com>
2026-03-17 19:03:20 +0100

    fix: CSP lack

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,29 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  async headers() {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
+
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; connect-src 'self' ${apiUrl}; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';`,          
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
